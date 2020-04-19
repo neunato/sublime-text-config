8f7ffcedfb302d3009a5bdd34c06bf1cc7cd22e7
Jan Ivica <wesleymustdie@gmail.com>
2020-04-19 22:15:08 +0200

    Add expand_selection_to_paragraph_sub command

---
+++ plugin_expand_selection_to_paragraph_sub.py
@@ -0,0 +1,26 @@
+from sublime import Region
+from sublime_plugin import TextCommand
+
+
+class ExpandSelectionToParagraphSubCommand(TextCommand):
+   def want_event(self):
+      return True
+
+   def run(self, edit, **args):
+      view = self.view
+      selection = view.sel()
+
+      pos = args["event"]
+      pos = (pos["x"], pos["y"])
+      pos = view.window_to_text(pos)
+      pos = Region(pos)
+
+      regs = list(selection)
+      selection.clear()
+      selection.add(pos)
+      view.run_command("expand_selection_to_paragraph", args)
+      reg = selection[0]
+
+      selection.clear()
+      selection.add_all(regs)
+      selection.subtract(reg)
