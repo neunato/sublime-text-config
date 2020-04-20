de0a277e9b4fc50e97911ca79ad60473f9540e52
Jan Ivica <wesleymustdie@gmail.com>
2020-04-20 13:53:51 +0200

    Fix trim_file command

--- plugin_trim_file.py
+++ plugin_trim_file.py
@@ -1,4 +1,4 @@
-import sublime
+from sublime import Region
 from sublime_plugin import TextCommand, EventListener
 
 
@@ -12,11 +12,18 @@ class TrimFileCommand(TextCommand):
 
       if eof:
          # reimplement ensure_newline_at_eof_on_save as it fires before on_pre_save
+         # also taking care of bug when cursor is at eof and \n ends up selected
          reg = view.find("\s*\Z", 0)
          settings = view.settings()
-         eof_newline = settings.get("ensure_newline_at_eof_on_save")
+         eof_newline = settings.get("_ensure_newline_at_eof_on_save")
          if eof_newline:
             view.replace(edit, reg, "\n")
+            s = view.size()
+            n = Region(s - 1, s)
+            selection = view.sel()
+            if selection.contains(n):
+               selection.subtract(n)
+               selection.add(Region(s))
          else:
             view.replace(edit, reg, "")
 
@@ -24,6 +31,13 @@ class TrimFileCommand(TextCommand):
 class TrimFileListener(EventListener):
    def on_pre_save(self, view):
       settings = view.settings()
+      eof_newline = settings.get("ensure_newline_at_eof_on_save")
+      if eof_newline is None:
+         eof_newline = settings.get("_ensure_newline_at_eof_on_save")
+      else:
+         settings.set("ensure_newline_at_eof_on_save", None)
+         settings.set("_ensure_newline_at_eof_on_save", eof_newline)
+
       trim = settings.get("_trim_file_on_save")
       if trim:
          view.run_command("trim_file")
