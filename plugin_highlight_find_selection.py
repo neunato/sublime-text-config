5e27a42a22789370539b7ff2adc03d393ff1d6d9
Jan Ivica <wesleymustdie@gmail.com>
2020-06-16 20:31:12 +0200

    Fix _highlight_find_selection setting

--- plugin_highlight_find_selection.py
+++ plugin_highlight_find_selection.py
@@ -9,25 +9,25 @@ class HighlightFindSelection(EventListener):
    Controlled by _highlight_find_selection setting.
    """
    def on_window_command(self, window, command, args):
-      if command != "show_panel":
-         return
+      if command == "show_panel":
+         panel = args.get("panel")
+         if panel not in ("find", "replace", "find_in_files", "incremental_find"):
+            views = window.views()
+            for view in views:
+               view.erase_regions("_find_selection")
+            return
 
-      panel = args.get("panel")
-      if panel not in ("find", "replace", "find_in_files", "incremental_find"):
-         return
+         view = window.active_view()
+         settings = view.settings()
+         highlight = settings.get("_highlight_find_selection")
+         if not highlight:
+            return
 
-      view = window.active_view()
-      settings = view.settings()
-      highlight = settings.get("_highlight_find_selection")
-      if not highlight:
-         return
+         sels = view.sel()
+         view = window.active_view()
+         view.add_regions("_find_selection", sels, scope="comment", flags=DRAW_NO_FILL)
 
-      sels = view.sel()
-      view = window.active_view()
-      view.add_regions("_find_selection", sels, scope="comment", flags=DRAW_NO_FILL)
-
-   def on_post_window_command(self, window, command, args):
-      if command != "hide_panel":
-         return
-      view = window.active_view()
-      view.erase_regions("_find_selection")
+      if command == "hide_panel":
+         views = window.views()
+         for view in views:
+            view.erase_regions("_find_selection")
