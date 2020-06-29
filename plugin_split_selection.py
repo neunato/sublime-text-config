1a8e47bb1b3cecb288824bfa4a16a7a006c5725a
Jan Ivica <wesleymustdie@gmail.com>
2020-06-29 13:53:09 +0200

    Extend split_selection command

--- plugin_split_selection.py
+++ plugin_split_selection.py
@@ -24,13 +24,28 @@ class SplitSelectionIntoChars(TextCommand):
 class SplitSelection(TextCommand):
    """
    Run split_selection_into_lines or split_selection_into_chars if nothing changed.
-   """
-   def run(self, edit):
-      view = self.view
-      sel = view.sel()
 
-      n = len(sel)
+   Optionally ignore empty lines.
+   """
+   def run(self, edit, empty=True):
+      view = self.view
+      sels = view.sel()
+      init = list(sels)
+
       view.run_command("split_selection_into_lines")
-      m = len(sel)
-      if n == m:
+
+      # remove empty lines
+      if not empty:
+         regs = list(sels)
+         for sel in regs:
+            line = view.line(sel)
+            if line.empty():
+               sels.subtract(sel)
+
+         # all lines empty, keep last selection
+         if len(sels) == 0:
+            sels.add(regs[-1])
+
+      # unaltered selections - split into chars
+      if init == list(sels):
          view.run_command("split_selection_into_chars")
