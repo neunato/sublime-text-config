5f2cb73d5e3c7ed2ecaf332f8de817973c8dfa8d
Jan Ivica <wesleymustdie@gmail.com>
2020-07-02 21:15:10 +0200

    Fix extended set_layout command

--- plugin_extend_set_layout.py
+++ plugin_extend_set_layout.py
@@ -1,29 +1,45 @@
 from sublime import set_timeout
-from sublime_plugin import EventListener
+from sublime_plugin import WindowCommand, EventListener
 
 
 class SetLayout(EventListener):
    """
    Move active and all tabs to the right to the newly created group, if any.
    """
-   active = None
-
    def on_window_command(self, window, command, args):
-      if command == "set_layout":
-         self.active = window.active_sheet()
+      if command == "set_layout" and args and args.get("transfer"):
+         del args["transfer"]
+         return "private_set_layout", args
 
-   def on_post_window_command(self, window, command, args):
-      if command == "set_layout":
-         active = self.active
-         g, i = window.get_sheet_index(active)
-         h = window.active_group()
-         n = window.num_groups() - 1
-         if g == -1:
-            return
-         if g >= n:
-            return
-         sheets = window.sheets_in_group(g)
-         sheets = sheets[i:]
-         for j, sheet in enumerate(sheets):
-            window.set_sheet_index(sheet, h, j)
-         set_timeout(lambda: window.focus_sheet(active), 1)
+
+class PrivateSetLayout(WindowCommand):
+   def run(self, **args):
+      window = self.window
+
+      sheet = window.active_sheet()
+      g, i = window.get_sheet_index(sheet)
+
+      window.run_command("set_layout", args)
+
+      h = window.num_groups() - 1
+
+      if g == -1:
+         return
+      if g >= h:
+         return
+
+      o = window.sheets_in_group(h)
+      o = len(o)
+
+      sheets = window.sheets_in_group(g)
+      sheets = sheets[i:]
+      for j, s in enumerate(sheets):
+         window.set_sheet_index(s, h, j + o)
+
+      sheets = window.sheets_in_group(g)
+      last_in_group = not sheets
+
+      if last_in_group:
+         set_timeout(lambda: window.focus_sheet(sheet))
+      else:
+         window.focus_sheet(sheet)
